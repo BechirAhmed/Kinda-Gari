17a8b9507e25a1a0010306cf0ebf07ebe3cc4f06
CKiilu <cndeti@gmail.com>
2016-03-06 10:41:25 +0300

    providing access to items in dataset

--- app/src/main/java/com/scurrae/chris/kindagari/MainActivity.java
+++ app/src/main/java/com/scurrae/chris/kindagari/MainActivity.java
@@ -3,12 +3,26 @@ package com.scurrae.chris.kindagari;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        // improving performance if change in content doesn't affect layout size of recyclerview
+        mRecyclerView.setHasFixedSize(true);
+        // Use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        // specify adapter
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
